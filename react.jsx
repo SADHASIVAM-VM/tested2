892636cd26e50f93eecf16a3e2d2d4b627f42f8b
SADHASIVAM-VM <sadha4747@gmail.com>
2023-10-19 06:27:34 -0700

    siva

--- react.jsx
+++ react.jsx
@@ -1,1 +1,12 @@
-this is created for react.jsx
+// this is created for react.jsx
+
+function App() {
+    return(
+        <div className="new">
+            <h1>hello</h1>
+        </div>
+
+    )
+  
+}
+<App/>
